fe8430e9d5b0d53714349bedbe2c331814c98265
Wenshan <ww2468@columbia.edu>
2018-02-14 20:27:32 -0500

    update

--- app/server.R
+++ app/server.R
@@ -35,93 +35,89 @@ server<- function(input, output){
   ## Part 2
   
   
-  observeEvent(input$occupation,
-               ## 2D map
-               
-               output$usmap <- renderLeaflet({
-                 
-                 ## subset the data
-                 #US = data.frame(Country = "US",longitude = -95.71289,latitude = 37.09024)
-                 ##### subset dataframe
-                 tmp <- data
-                 
-                 tmp<-subset(data,OCC_TITLE == as.character(input$occupation))
-                 
-                 if(nrow(tmp)>0){
-                   
-                   ###### 
-                   
-                   # SetColor <- function(tmp) {
-                   #   sapply(tmp$A_MEAN, function(wage) {
-                   #     if(wage <= 30000) {
-                   #       "lightgray"
-                   #     } else if(wage <= 40000) {
-                   #       "lightblue"
-                   #     } else if(wage <= 50000) {
-                   #       "blue"
-                   #     } else if(wage <= 70000) {
-                   #       "darkblue"
-                   #     } else {
-                   #       "black"
-                   #     } })
-                   # }
-                   # 
-                   # icons <- awesomeIcons(
-                   #   icon = 'ios-close',
-                   #   iconColor = 'white',
-                   #   library = 'ion',
-                   #   markerColor = paste(SetColor(tmp))
-                   # )
-                   # 
-                   # 
-                   # 
-                   # Colors = c("lightgray","lightblue","blue","darkblue","black")
-                   # Labels = c("<= 30000","<= 40000","<= 50000","<= 70000","> 70000")
-                   # 
-                   # leaflet(tmp)%>%addProviderTiles("Esri.WorldStreetMap")%>%
-                   #   addAwesomeMarkers(~lon, ~lat, icon=icons, label=~as.character(A_MEAN))%>%  
-                   #   setView(lng=-30,lat=28,zoom=2)%>%#put US in the centre
-                   #   addLegend("topright", colors = Colors, labels = Labels,
-                   #             title = "Wage Level<br/>From Low to High",
-                   #             labFormat = labelFormat(prefix = "$"),
-                   #             opacity = 1)
-                   
-                   pal <- colorNumeric(palette="YlGnBu", domain=tmp$A_MEAN)
-                   
-                   mapStates = map("state", fill = TRUE, plot = FALSE)
-                   
-                   labels <- sprintf(
-                     "<strong>%s</strong><br/>%g Annual Salary",
-                     tmp$STATE, tmp$A_MEAN) %>% 
-                     lapply(htmltools::HTML)
-                   
-                   leaflet(data = mapStates) %>% addTiles() %>%
-                     addPolygons(fillColor = ~pal(tmp$A_MEAN), 
-                                 weight = 2, 
-                                 opacity = 1, 
-                                 color = "white",
-                                 dashArray = "3", 
-                                 fillOpacity = 0.7, 
-                                 highlight = highlightOptions(
-                                   weight = 5,
-                                   color = "#666",
-                                   dashArray = "",
-                                   fillOpacity = 0.7,
-                                   bringToFront = TRUE),
-                                 label = labels,
-                                 labelOptions = labelOptions(
-                                   style = list("font-weight" = "normal", 
-                                                padding = "3px 8px"),
-                                   textsize = "15px",
-                                   direction = "auto")) %>%
-                     addLegend("bottomright", pal = pal, values = ~tmp$A_MEAN,
-                               title = "Salary Level",
-                               labFormat = labelFormat(prefix = "$"),
-                               opacity = 1)
-                 }
-                 
-               })
-               
+  ## 2D map
+  
+  output$usmap <- renderLeaflet({
+    
+    tmp <- data
+    
+    tmp<-subset(data,OCC_TITLE == as.character(input$occupation))
+    
+    if(nrow(tmp)>0){
+      
+      ###### 
+      
+      # SetColor <- function(tmp) {
+      #   sapply(tmp$A_MEAN, function(wage) {
+      #     if(wage <= 30000) {
+      #       "lightgray"
+      #     } else if(wage <= 40000) {
+      #       "lightblue"
+      #     } else if(wage <= 50000) {
+      #       "blue"
+      #     } else if(wage <= 70000) {
+      #       "darkblue"
+      #     } else {
+      #       "black"
+      #     } })
+      # }
+      # 
+      # icons <- awesomeIcons(
+      #   icon = 'ios-close',
+      #   iconColor = 'white',
+      #   library = 'ion',
+      #   markerColor = paste(SetColor(tmp))
+      # )
+      # 
+      # 
+      # 
+      # Colors = c("lightgray","lightblue","blue","darkblue","black")
+      # Labels = c("<= 30000","<= 40000","<= 50000","<= 70000","> 70000")
+      # 
+      # leaflet(tmp)%>%addProviderTiles("Esri.WorldStreetMap")%>%
+      #   addAwesomeMarkers(~lon, ~lat, icon=icons, label=~as.character(A_MEAN))%>%  
+      #   setView(lng=-30,lat=28,zoom=2)%>%#put US in the centre
+      #   addLegend("topright", colors = Colors, labels = Labels,
+      #             title = "Wage Level<br/>From Low to High",
+      #             labFormat = labelFormat(prefix = "$"),
+      #             opacity = 1)
+      
+      pal <- colorNumeric(palette="YlGnBu", domain=tmp$A_MEAN)
+      
+      mapStates = map("state", fill = TRUE, plot = FALSE)
+      
+      labels <- sprintf(
+        "<strong>%s</strong><br/>$ %g Annual Salary",
+        tmp$STATE, tmp$A_MEAN) %>% 
+        lapply(htmltools::HTML)
+      
+      leaflet(data = mapStates) %>% addTiles() %>%
+        addPolygons(fillColor = ~pal(tmp$A_MEAN), 
+                    weight = 2, 
+                    opacity = 1, 
+                    color = "white",
+                    dashArray = "3", 
+                    fillOpacity = 0.7, 
+                    highlight = highlightOptions(
+                      weight = 5,
+                      color = "#666",
+                      dashArray = "",
+                      fillOpacity = 0.7,
+                      bringToFront = TRUE),
+                    label = labels,
+                    labelOptions = labelOptions(
+                      style = list("font-weight" = "normal", 
+                                   padding = "3px 8px"),
+                      textsize = "15px",
+                      direction = "auto")) %>%
+        addLegend("bottomright", pal = pal, values = ~tmp$A_MEAN,
+                  title = "Salary Level",
+                  labFormat = labelFormat(prefix = "$"),
+                  opacity = 1)
+    }
+    
+  }
+  
   )
 }
 ## end 2D map
